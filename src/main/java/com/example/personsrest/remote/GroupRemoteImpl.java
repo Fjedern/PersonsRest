fe09a699d6f3c3cc60612d868e8c5965cbd5f9d3
David <david@nlsn.nu>
2022-01-20 09:48:21 +0100

    keycloaktoken added

--- src/main/java/com/example/personsrest/remote/GroupRemoteImpl.java
+++ src/main/java/com/example/personsrest/remote/GroupRemoteImpl.java
@@ -1,9 +1,24 @@
 package com.example.personsrest.remote;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
 public class GroupRemoteImpl implements GroupRemote{
+
+    private static final String BASE_URL = "api/groups";
+
+    WebClient webClient;
+
     @Override
     public String getNameById(String groupId) {
-        return null;
+        return webClient.get()
+                .uri(BASE_URL)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(Mono.just(empl), Employee.class)
+                .retrieve()
+                .bodyToMono(Group.class);
+        ;
     }
 
     @Override
